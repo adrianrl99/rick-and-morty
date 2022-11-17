d7eabccd7dee8fe9d77e1a34972b31a95223e49e
Xeraph <61700204+adrianrl99@users.noreply.github.com>
2022-11-17 01:13:35 -0500

    Initial Commit

---
+++ rick-and-morty/rick_and_mortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rick_and_mortyApp.swift
+//  rick-and-morty
+//
+//  Created by Macbook on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct rick_and_mortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
